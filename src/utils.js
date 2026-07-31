7fa5eb3048174cb7335156b2279fbeca569231e2
Carolina Betlienski <scarfcarob@gmail.com>
2026-07-31 15:48:03 -0300

    feat(m3-utils): agregar funcion auxiliar para formatear hora de mensajes

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+
+export function obtenerHoraActual() {
+  const ahora = new Date();
+  const horas = String(ahora.getHours()).padStart(2, '0');
+  const minutos = String(ahora.getMinutes()).padStart(2, '0');
+  return `${horas}:${minutos}`;
+}
+

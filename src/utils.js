a48904d83beeffc911040f77b5120b46bb4c51b7
Carolina Betlienski <scarfcarob@gmail.com>
2026-08-11 00:59:04 -0300

    fix(m3): agregar .env y carpeta .vercel al gitignore

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,8 @@ export function obtenerHoraActual() {
   return `${horas}:${minutos}`;
 }
 
+export function escapeHTML(str) {
+  return str.replace(/[&<>'"]/g,
+    tag => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag)
+  );
+}
